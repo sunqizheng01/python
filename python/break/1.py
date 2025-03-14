00121a1a0a74fe7a4c5a95edd94274e219cfe68c
sunqizheng01 <89573771+sunqizheng01@users.noreply.github.com>
2025-03-14 17:02:11 +0800

    Create 1.py

---
+++ python/break/1.py
@@ -0,0 +1,11 @@
+#基础
+
+#print("hello world.")
+
+#help('sum')
+
+#print('''这是一段多行字符串。这是它的第一行。This is the second line."What's your name?," I asked.He said "Bond, James Bond."''')
+
+#print('{0:.3f}'.format(1.0/3))
+
+#print("{}".format('sunqz'))
